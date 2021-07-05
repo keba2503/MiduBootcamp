835b33856442e0006210f7749d8a40e1f0022c41
Karen Borrero <keba2503@gmail.com>
2021-07-05 13:30:28 +0100

    edit first component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Mensaje from './components/mensaje';
+
+const Description = () => {
+  return <p> Esta es la primera practica de react</p>
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Mensaje />
+     <Description />
+
     </div>
   );
 }
---
+++ src/components/mensaje.js
@@ -0,0 +1,6 @@
+const Mensaje = () => {
+    return <h1> Estamos probando crear components</h1>
+  }
+  
+
+  export default Mensaje;
